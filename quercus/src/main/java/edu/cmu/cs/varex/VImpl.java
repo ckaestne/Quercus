f63db4ccc9b3fddfe895a896fc66f16a7cac5bb2
Christian Kaestner <kaestner@cs.cs.cmu.edu>
2015-11-28 12:32:16 -0500

    change VImpl to always share values
    maximal sharing in V structure

--- quercus/src/main/java/edu/cmu/cs/varex/VImpl.java
+++ quercus/src/main/java/edu/cmu/cs/varex/VImpl.java
@@ -12,19 +12,19 @@ import java.util.function.*;
 class VImpl<T> implements V<T> {
 
     static <U> V<U> choice(FeatureExpr condition, U a, U b) {
-        Map<FeatureExpr, U> result = new HashMap<>(2);
+        Map<U, FeatureExpr> result = new HashMap<>(2);
         if (condition.isSatisfiable())
-            result.put(condition, a);
+            put(result, a, condition);
         else return V.one(b);
         if (condition.not().isSatisfiable())
-            result.put(condition.not(), b);
+            put(result, b, condition.not());
         else return V.one(a);
 
         return new VImpl<>(result);
     }
 
     static <U> V<? extends U> choice(FeatureExpr condition, V<? extends U> a, V<? extends U> b) {
-        Map<FeatureExpr, U> result = new HashMap<>(2);
+        Map<U, FeatureExpr> result = new HashMap<>(2);
         if (condition.isSatisfiable())
             addVToMap(result, condition, a);
         else return b;
@@ -32,21 +32,22 @@ class VImpl<T> implements V<T> {
             addVToMap(result, condition.not(), a);
         else return a;
 
-        return new VImpl<>(result);
+        return createV(result);
     }
 
-    private VImpl(Map<FeatureExpr, T> values) {
+    private VImpl(Map<T, FeatureExpr> values) {
         this.values = values;
-        assert checkInvariant(): "invariants violated";
+        assert checkInvariant() : "invariants violated";
     }
 
     //invariant: nonempty, all FeatureExpr together yield true
-    private final Map<FeatureExpr, T> values;
+    private final Map<T, FeatureExpr> values;
 
     private boolean checkInvariant() {
         if (values.isEmpty()) return false;// : "empty V";
+        if (values.size()<2) return false;// : "singleton VImpl?";
         FeatureExpr conditions = FeatureExprFactory.False();
-        for (FeatureExpr cond : values.keySet()) {
+        for (FeatureExpr cond : values.values()) {
             if (!conditions.and(cond).isContradiction()) return false;// : "condition overlaps with previous condition";
             conditions = conditions.or(cond);
         }
@@ -59,75 +60,93 @@ class VImpl<T> implements V<T> {
     @Override
     public T getOne() {
         assert false : "getOne called on Choice: " + this;
-        return values.values().iterator().next();
+        return values.keySet().iterator().next();
     }
+
 
     @Override
     public <U> V<? extends U> map(Function<? super T, ? extends U> fun) {
-        Map<FeatureExpr, U> result = new HashMap<>(values.size());
-        for (HashMap.Entry<FeatureExpr, T> e : values.entrySet())
-            result.put(e.getKey(), fun.apply(e.getValue()));
-        return new VImpl<>(result);
+        Map<U, FeatureExpr> result = new HashMap<>(values.size());
+        for (HashMap.Entry<T, FeatureExpr> e : values.entrySet())
+            put(result, fun.apply(e.getKey()), e.getValue());
+        return createV(result);
     }
 
     @Override
     public <U> V<? extends U> vmap(FeatureExpr ctx, BiFunction<FeatureExpr, ? super T, ? extends U> fun) {
-        Map<FeatureExpr, U> result = new HashMap<>(values.size());
-        for (HashMap.Entry<FeatureExpr, T> e : values.entrySet())
-            result.put(e.getKey(), fun.apply(ctx.and(e.getKey()), e.getValue()));
-        return new VImpl<>(result);
+        Map<U, FeatureExpr> result = new HashMap<>(values.size());
+        for (HashMap.Entry<T, FeatureExpr> e : values.entrySet())
+            put(result, fun.apply(ctx.and(e.getValue()), e.getKey()), e.getValue());
+        return createV(result);
     }
 
     @Override
     public <U> V<? extends U> flatMap(Function<? super T, V<? extends U>> fun) {
-        Map<FeatureExpr, U> result = new HashMap<>(values.size());
-        for (HashMap.Entry<FeatureExpr, T> e : values.entrySet()) {
-            V<? extends U> u = fun.apply(e.getValue());
-            addVToMap(result, e.getKey(), u);
+        Map<U, FeatureExpr> result = new HashMap<>(values.size());
+        for (HashMap.Entry<T, FeatureExpr> e : values.entrySet()) {
+            V<? extends U> u = fun.apply(e.getKey());
+            addVToMap(result, e.getValue(), u);
         }
-        return new VImpl<>(result);
+        return createV(result);
     }
+
 
     @Override
     public <U> V<? extends U> vflatMap(FeatureExpr ctx, BiFunction<FeatureExpr, ? super T, V<? extends U>> fun) {
-        Map<FeatureExpr, U> result = new HashMap<>(values.size());
-        for (HashMap.Entry<FeatureExpr, T> e : values.entrySet()) {
-            V<? extends U> u = fun.apply(ctx.and(e.getKey()), e.getValue());
-            addVToMap(result, e.getKey(), u);
+        Map<U, FeatureExpr> result = new HashMap<>(values.size());
+        for (HashMap.Entry<T, FeatureExpr> e : values.entrySet()) {
+            V<? extends U> u = fun.apply(ctx.and(e.getValue()), e.getKey());
+            addVToMap(result, e.getValue(), u);
         }
-        return new VImpl<>(result);
+        return createV(result);
     }
 
-    private static <U> void addVToMap(Map<FeatureExpr, U> result, FeatureExpr ctx, V<? extends U> u) {
+    private static <U> void addVToMap(Map<U, FeatureExpr> result, FeatureExpr ctx, V<? extends U> u) {
         assert (u instanceof One) || (u instanceof VImpl) : "unexpected V value: " + u;
         if (u instanceof One)
-            result.put(ctx, ((One<U>) u).value);
+            put(result, ((One<U>) u).value, ctx);
         else
-            for (HashMap.Entry<FeatureExpr, U> ee : ((VImpl<U>) u).values.entrySet()) {
-                FeatureExpr cond = ctx.and(ee.getKey());
+            for (HashMap.Entry<U, FeatureExpr> ee : ((VImpl<U>) u).values.entrySet()) {
+                FeatureExpr cond = ctx.and(ee.getValue());
                 if (cond.isSatisfiable())
-                    result.put(cond, ee.getValue());
+                    put(result, ee.getKey(), cond);
             }
+    }
+
+    private static <U> void put(Map<U, FeatureExpr> map, U value, FeatureExpr condition) {
+        if (map.containsKey(value))
+            condition = condition.or(map.get(value));
+        map.put(value, condition);
+    }
+
+    private static <U> V<? extends U> createV(Map<U, FeatureExpr> map) {
+        assert map.size() > 0 : "empty result";
+        if (map.size() == 1) {
+            assert map.values().iterator().next().isTautology() : "incomplete result " + map;
+            return new One(map.keySet().iterator().next());
+        }
+        return new VImpl<>(map);
     }
 
     @Override
     public void foreach(Consumer<T> fun) {
-        values.values().forEach(fun::accept);
+        values.keySet().forEach(fun::accept);
 
     }
 
+
     @Override
     public void vforeach(FeatureExpr ctx, BiConsumer<FeatureExpr, T> fun) {
-        for (HashMap.Entry<FeatureExpr, T> e : values.entrySet())
-            fun.accept(ctx.and(e.getKey()), e.getValue());
+        for (HashMap.Entry<T, FeatureExpr> e : values.entrySet())
+            fun.accept(ctx.and(e.getValue()), e.getKey());
     }
 
     @Override
     public FeatureExpr when(Predicate<T> condition) {
         FeatureExpr result = FeatureExprFactory.False();
-        for (HashMap.Entry<FeatureExpr, T> e : values.entrySet())
-            if (condition.test(e.getValue()))
-                result = result.or(e.getKey());
+        for (HashMap.Entry<T, FeatureExpr> e : values.entrySet())
+            if (condition.test(e.getKey()))
+                result = result.or(e.getValue());
         return result;
     }
 
@@ -135,8 +154,8 @@ class VImpl<T> implements V<T> {
     public String toString() {
         StringBuffer out = new StringBuffer();
         List<String> entries = new ArrayList<>(values.size());
-        for (HashMap.Entry<FeatureExpr, T> e : values.entrySet())
-            entries.add(e.getValue() + "<-" + e.getKey().toTextExpr());
+        for (HashMap.Entry<T, FeatureExpr> e : values.entrySet())
+            entries.add(e.getKey() + "<-" + e.getValue().toTextExpr());
         Collections.sort(entries);
         out.append("CHOICE(");
         for (String e : entries) {
