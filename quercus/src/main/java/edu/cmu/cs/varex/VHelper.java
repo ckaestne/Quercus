33d457a5437c1d490cd782945b574cdb077fc78f
Christian Kaestner <kaestner@cs.cs.cmu.edu>
2015-11-28 06:22:16 -0500

    implement mapAll variations

--- quercus/src/main/java/edu/cmu/cs/varex/VHelper.java
+++ quercus/src/main/java/edu/cmu/cs/varex/VHelper.java
@@ -12,18 +12,18 @@ public class VHelper {
         return FeatureExprFactory.True();
     }
 
-    public static <A,B,C,R> V<R> mapAll(V<A> a, V<B> b, V<C> c, Function4<A, B, C, R> fun) {
-        System.err.println("missing implementation mapAll(a,b,c)");
-        return toV(fun.apply(a.getOne(),b.getOne(),c.getOne()));
+    public static <A, B, C, R> V<R> mapAll(V<A> a, V<B> b, V<C> c, Function4<A, B, C, R> fun) {
+        return a.flatMap((aa) ->
+                b.flatMap((bb) ->
+                        c.map((cc) -> fun.apply(aa, bb, cc))));
     }
 
-    public static <A,B,R> V<R> mapAll(V<A> a, V<B> b, Function3<A, B, R> fun) {
-        System.err.println("missing implementation mapAll(a,b)");
-        return toV(fun.apply(a.getOne(),b.getOne()));
+    public static <A, B, R> V<R> mapAll(V<A> a, V<B> b, Function3<A, B, R> fun) {
+        return a.flatMap((aa) ->
+                b.map((bb) -> fun.apply(aa, bb)));
     }
 
     public static <T> V<T> toV(T v) {
-        System.err.println("missing implementation toV");
         return V.one(v);
     }
 }
