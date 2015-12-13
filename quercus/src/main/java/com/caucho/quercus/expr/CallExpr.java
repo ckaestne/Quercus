c8f59c6b27c19ad400e8b07b64b68a80a3eb9d14
Christian Kaestner <kaestner@cs.cs.cmu.edu>
2015-12-13 06:36:56 -0500

    deactivate temporary logging again

--- quercus/src/main/java/com/caucho/quercus/expr/CallExpr.java
+++ quercus/src/main/java/com/caucho/quercus/expr/CallExpr.java
@@ -197,7 +197,7 @@ public class CallExpr extends Expr {
 //      w.flush();
 //    } catch (IOException e) {
 //    }
-    System.out.println(this.toString());
+//    System.out.println(this.toString());
     if (_funId==null)
       _funId=lookupFunId(env);
 
